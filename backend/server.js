1b70a0f84a60ffa83b3b2fe2507407d4b034f337
Nathan Fernandes Achite <109240367+Nathanfernds@users.noreply.github.com>
2025-12-01 16:18:36 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,34 +6,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Conexão com banco
 const db = require("./config/db");
 
 app.get("/", (req, res) => {
   res.send("API do Cardápio Digital está funcionando!");
 });
 
-// ROTAS NORMAIS
 const categoriasRoutes = require("./routes/categorias");
 app.use("/categorias", categoriasRoutes);
 
 const itensRoutes = require("./routes/itens");
 app.use("/itens", itensRoutes);
 
-// LOGIN ADMIN (SEM AUTH)
 const adminRoutes = require("./routes/admin");
 app.use("/admin", adminRoutes);
 
-// AUTH MIDDLEWARE (somente para partes protegidas)
 const auth = require("./middleware/auth");
 
-// ROTAS DE PEDIDOS
 const pedidosRoutes = require("./routes/pedidos");
-app.use("/pedidos", pedidosRoutes);  // POST público
-app.use("/admin/pedidos", auth, pedidosRoutes); // GET/PUT protegido
+app.use("/pedidos", pedidosRoutes);  
+app.use("/admin/pedidos", auth, pedidosRoutes); 
 
-// ROTAS PROTEGIDAS PARA CRUD ITENS
-app.use("/admin/itens", auth, itensRoutes);  // POST / PUT / DELETE
+app.use("/admin/itens", auth, itensRoutes);  
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
